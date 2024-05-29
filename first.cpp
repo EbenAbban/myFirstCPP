700701784357306315ffa088e443606f6f33c35c
EbenAbban <abbaneben07@gmail.com>
2024-05-29 14:32:06 +0000

    I removed c

--- first.cpp
+++ first.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int a,b;
-    cout<<"enter a number";
-    cin>>a;
-     cout<<"enter a number";
-    cin>>b;
-    cout<< a + b + c;
+int main() {
+  int a, b;
+  cout << "enter a number";
+  cin >> a;
+  cout << "enter a number";
+  cin >> b;
+  cout << a + b;
 }
